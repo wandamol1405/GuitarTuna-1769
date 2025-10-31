2f40e81a2732bc1b9fdb5ecc6c0fb196913789ae
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2025-10-31 12:11:05 -0300

    Add microphone test

--- modules-tests/ky037-test.c
+++ modules-tests/ky037-test.c
@@ -5,69 +5,49 @@
 #include "lpc17xx_adc.h"
 
 void cfgGPIO(){
-	PINSEL_CFG_Type pinGPIO;
-	pinGPIO.Portnum = PINSEL_PORT_0;
-	pinGPIO.Pinnum = PINSEL_PIN_22;
-	pinGPIO.Funcnum = PINSEL_FUNC_0;
+	PINSEL_CFG_Type pinGPIO = {0};
+	pinGPIO.Portnum = 0;
+	pinGPIO.Pinnum = 22;
+	pinGPIO.Funcnum = 0;
 	pinGPIO.Pinmode = PINSEL_PINMODE_TRISTATE;
 	pinGPIO.OpenDrain = PINSEL_PINMODE_NORMAL;
-    GPIO_SetDir(0, (1<<22), 1);
-	LPC_GPIO0->FIOCLR |= (1<<22);
+    GPIO_SetDir(0,22, 1);
+	LPC_GPIO0->FIOSET |= (1<<22);
 	LPC_GPIO1->FIOSET |= (1<<25);
 	LPC_GPIO1->FIOSET |= (1<<26);
 	PINSEL_ConfigPin(&pinGPIO);
 }
 
 void cfgADC(){
-	PINSEL_CFG_Type pinADC;
-	pinADC.Portnum = PINSEL_PORT_0;
-	pinADC.Pinnum = PINSEL_PIN_23;
-	pinADC.Funcnum = PINSEL_FUNC_1;
+	PINSEL_CFG_Type pinADC = {0};
+	pinADC.Portnum = 0;
+	pinADC.Pinnum = 23;
+	pinADC.Funcnum = 1;
 	pinADC.Pinmode = PINSEL_PINMODE_TRISTATE;
 	pinADC.OpenDrain = PINSEL_PINMODE_NORMAL;
 	PINSEL_ConfigPin(&pinADC);
 
 	ADC_Init(LPC_ADC, 200000);
-	ADC_EdgeStartConfig(LPC_ADC, ADC_START_ON_RISING);
 	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
 	ADC_IntConfig(LPC_ADC, ADC_CHANNEL_0, ENABLE);
 	NVIC_EnableIRQ(ADC_IRQn);
-	ADC_BurstCmd(LPC_ADC, DISABLE);
-	ADC_StartCmd(LPC_ADC, ADC_START_ON_MAT01);
+	ADC_BurstCmd(LPC_ADC, ENABLE);
+	//ADC_StartCmd(LPC_ADC, ADC_START_ON_MAT01);
 }
 
-void cfgTimer(){
-	TIM_TIMERCFG_Type timer;
-	timer.PrescaleOption = TIM_PRESCALE_USVAL;
-	timer.PrescaleValue = 1000000;
-
-	TIM_MATCHCFG_Type match;
-	match.MatchChannel = 1;
-	match.IntOnMatch = DISABLE;
-	match.ResetOnMatch = ENABLE;
-	match.StopOnMatch = DISABLE;
-	match.ExtMatchOutputType = TIM_EXTMATCH_TOGGLE;
-	match.MatchValue = 60;
-
-	TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &timer);
-	TIM_ConfigMatch(LPC_TIM0, &match);
-	TIM_Cmd(LPC_TIM0, ENABLE);
-}
 
 int main(){
 	cfgGPIO();
-	cfgTimer();
 	cfgADC();
 	while(1);
 }
 
 void ADC_IRQHandler(){
-	LPC_GPIO0->FIOPIN ^= (1<<22);
-//	uint16_t value = ADC_ChannelGetData(LPC_ADC, ADC_CHANNEL_0);
-//	if(value > 0x800){
-//		LPC_GPIO0->FIOSET |= (1<<6);
-//	}else{
-//		LPC_GPIO0->FIOCLR |= (1<<6);
-//	}
+	uint16_t value = ADC_ChannelGetData(LPC_ADC, ADC_CHANNEL_0);
+	if(value > 0x800){
+		LPC_GPIO0->FIOSET |= (1<<22);
+	}else{
+		LPC_GPIO0->FIOCLR |= (1<<22);
+	}
 	NVIC_ClearPendingIRQ(ADC_IRQn);
 }
